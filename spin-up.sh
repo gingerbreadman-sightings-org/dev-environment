38e3373991a1958986a5c23259081b7f0b0434af
Daniel Kreuer <d.kreuer@ibrams.com>
2015-03-18 14:58:15 +0100

    Initial commit

---
+++ spin-up.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ ! -e ./gingerbreadman-sightings.org ]; then
+    git clone https://github.com/gingerbreadman-sightings-org/gingerbreadman-sightings.org
+fi
+
+vagrant up
+vagrant ssh -c "if [ ! -e /var/www/gingerbreadman-sightings.org/app/config/parameters.yml ]; then cp /var/www/spin-up-files/parameters.yml /var/www/gingerbreadman-sightings.org/app/config/parameters.yml; fi;"
+vagrant ssh -c "cd /var/www/gingerbreadman-sightings.org/ && composer install"
